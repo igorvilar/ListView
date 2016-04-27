a523ec176c50b3b5d1f88af1e76dff4f5c5c0546
trakimobile <igorvilar@outlook.com>
2016-04-27 15:43:58 -0300

    Add tableview heavy metal bands with alert to select

--- ListViewDemo/ViewController.swift
+++ ListViewDemo/ViewController.swift
@@ -8,18 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet var tableView: UITableView!
+    let swiftHeavyMetal = ["Alice Cooper", "Black Sabbath", "Deep Purple", "Iron Maiden", "Judas Priest", "Megadeth", "Metallica", "Motörhead"]
+    let textCellIdentifier = "TextCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return swiftHeavyMetal.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(textCellIdentifier, forIndexPath: indexPath)
+        
+        let row = indexPath.row
+        cell.textLabel?.text = swiftHeavyMetal[row]
+        
+        return cell
+    }
 
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+        let row = indexPath.row
+        print(swiftHeavyMetal[row])
+        let alert = UIAlertController(title: "Alert", message: swiftHeavyMetal[row], preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)    }
 
 }
 
